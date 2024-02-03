835b08705da9cd251ea5d972b269f69e0fecaf2f
BradlyeG <gordonbradlye@gmail.com>
2024-02-03 18:46:26 -0500

    build a better test
    counters should always be put in their own functions that trigger on an event :)

--- python/flow_meter_test.py
+++ python/flow_meter_test.py
@@ -11,6 +11,7 @@
 import gpiozero
 import sys
 from time import sleep
+from signal import pause
 
 # Pin Definitions
 IN_SOL_PIN = 23
@@ -24,26 +25,34 @@ PLS_CON = .0025
 # Devices and objects
 in_sol_ctrl = gpiozero.DigitalOutputDevice(IN_SOL_PIN)
 out_sol_ctrl = gpiozero.DigitalOutputDevice(OUT_SOL_PIN)
-in_flw_ctrl = gpiozero.DigitalInputDevice(IN_FLW_MTR, True) # True to enable internal pull up resistor on pi
-out_fwl_ctrl = gpiozero.DigitalInputDevice(OUT_FLW_MTR, True) # True to enable internal pull up resistor on pi
+in_flw_ctrl = gpiozero.DigitalInputDevice(IN_FLW_MTR, True, bounce_time=0.1) # True to enable internal pull up resistor on pi and account for bounce - time for signal to change properly
+out_fwl_ctrl = gpiozero.DigitalInputDevice(OUT_FLW_MTR, True, bounce_time=0.1) # True to enable internal pull up resistor on pi and account for bounce - time for signal to change properly
 
 # Web server's php will call this python script, grab the amount specified from the web dashboard, and pass in the water target as the first and only argument
 #water_target = float(sys.argv[1])
 water_amount = 0
 
 # Sensor outputs pulses when the hall effect sensor moves
-sensor_count = 0
+sensorp_count = 0
+
+# Simple function to count the pulses, and output how much water has been let out thus far
+def count_pulse():
+    global sensorp_count
+    sensorp_count += 1
+    print("Estimated Amount: " + str((sensorp_count * PLS_CON)) + " liters")
+
+# Give the sensor a when_activated function
+in_flw_ctrl.when_activated = count_pulse
 
 # The script only needs to run once. Best practice would put this all in a class but this is a test
 
 while True:
-    # Activate solenoid
-    in_sol_ctrl.on()
-    # Track the sensor count
-    if in_flw_ctrl.value:
-        sensor_count += 1
-    # print how many we have
-    print("Estimated Amount: " + str((sensor_count * PLS_CON)) + " liters")
+    # Activate solenoid if it isn't already
+    if not in_sol_ctrl.value:
+        in_sol_ctrl.on()
+    # The script still needs to run, but doesn't need to do anything in while waiting
+    pause()
+    
 
 
 
