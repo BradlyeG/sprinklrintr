7a8656cae6b6d513d0f809df4028c3dcb99f7edb
BradlyeG <gordonbradlye@gmail.com>
2024-02-05 11:15:51 -0500

    Integrate tests into main control program
    This is still a test effectively, the current development set up is only using one solenoid

--- python/sprinklr_control.py
+++ python/sprinklr_control.py
@@ -21,35 +21,71 @@ OUT_FLW_MTR = 15
 # Sensor pulse conversion per specs, in liters
 PLS_CON = .0025
 
+# Bounce time to calibrate liquid flow meters
+LFM_BNC = 0.001
+
+# Constant for watchdog timeout count
+WTD_TMOC = 3
+
 # Devices and objects
 in_sol_ctrl = gpiozero.DigitalOutputDevice(IN_SOL_PIN)
 out_sol_ctrl = gpiozero.DigitalOutputDevice(OUT_SOL_PIN)
-in_flw_ctrl = gpiozero.DigitalInputDevice(IN_FLW_MTR, True) # True to enable internal pull up resistor on pi
-out_fwl_ctrl = gpiozero.DigitalInputDevice(OUT_FLW_MTR, True) # True to enable internal pull up resistor on pi
+in_flw_ctrl = gpiozero.DigitalInputDevice(IN_FLW_MTR, True, bounce_time=LFM_BNC) # True to enable internal pull up resistor on pi and account for bounce - time for signal to change properly
+out_fwl_ctrl = gpiozero.DigitalInputDevice(OUT_FLW_MTR, True, bounce_time=LFM_BNC) # True to enable internal pull up resistor on pi and account for bounce - time for signal to change properly
 
 # Web server's php will call this python script, grab the amount specified from the web dashboard, and pass in the water target as the first and only argument
 water_target = float(sys.argv[1])
 water_amount = 0
 
 # Sensor outputs pulses when the hall effect sensor moves
-sensor_count = 0
+sensorp_count = 0
+lastsensorp_count = 0
+
+# How many times should the watchdog notice no flow output
+# before quitting
+timeout_count = 0
+
+# Simple function to count the pulses
+def count_pulse():
+    global sensorp_count, lastsensorp_count
+    lastsensorp_count = sensorp_count
+    sensorp_count += 1
+
+# Simple function to calculate the water amount
+def calculate_water():
+    global sensorp_count, water_amount
+    water_amount = sensorp_count * PLS_CON
+
+# The watch dog makes sure that the sensor is actually doing something
+# or quits the program if it isn't or there is some error
+def watchdog():
+    global sensorp_count, lastsensorp_count, timeout_count
+
+    if timeout_count == WTD_TMOC:
+        sys.exit("Watchdog no longer detecting water output")
+    elif lastsensorp_count == sensorp_count:
+        timeout_count += 1
+
+# Attach function to sensor's when_activated
+in_flw_ctrl.when_activated = count_pulse
 
 print("Water target is: " + str(water_target) + " L")
 
 # The script only needs to run once. Best practice would put this all in a class but this is a test
 while water_amount < water_target:
+
     # Activate the solenoid, only if it already off
     if not in_sol_ctrl.value:
         in_sol_ctrl.on()
-        print("sols on")
 
-    # If a pulse is detected
-    if in_flw_ctrl.value:
-        # Add to the total amount of pulses
-        sensor_count += 1
-        # Convert pulses to liters
-        water_amount = sensor_count * PLS_CON
-        print("Water amount: " + str(water_amount))
+    # Wait a moment
+    sleep(0.5)
+
+    # Update the water amount
+    calculate_water()
+
+    # Run the watchdog
+    watchdog()
 
     # We need to turn the solenoid off if we have watered enough
     if water_amount >= water_target:
